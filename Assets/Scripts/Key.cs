1096073870f9491f6596457c7b3acdcaad0be4d7
Martinesteso <49696739@est.ort.edu.ar>
2026-05-18 10:40:29 -0300

    prime

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -1,10 +1,24 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-    void OnMouseDown(){
+
+    bool pressed = false;
+    float pressOff = 1f;
+    void OnMouseDown()
+    {
         Debug.Log("Click en " + gameObject.name);
+        transform.Translate(0,-1,0);
+    }
+
+    void OnMouseExist(){
+
+
+    }
+
+    void OnMouseUp(){
+        transform.Translate(0,pressOff,0);
     }
 }
---
+++ Assets/Scripts/KeyboardManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardManager : MonoBehaviour
+{
+
+    public Key[] keys;
+
+    // Start is called
+    //  before the first frame update
+    void Start()
+    {
+        keys = GameObject.FindObjectsOfType<Key>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.Space))
+    {
+        int i = Random.Range(0, keys.Length);
+        Destroy(keys[i].gameObject);
+    }
+}}
